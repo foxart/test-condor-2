3ab8eb6182a283e3280648c838504a5f4bb6c747
ivankosenko <ivan.kosenko@uvision.co.il>
2025-02-21 17:19:04 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+function debug(mixed $data, string $class = 'debug'): void
+{
+    if ($class === 'error'){
+        $style = 'color: red;';
+    } elseif ($class === 'info') {
+        $style = 'color: blue;';
+    } else {
+        $style = 'color: gray;';
+    }
+    if (is_array($data) || is_object($data))
+    echo "<pre style='$style'>";
+    print_r($data);
+    echo "</pre>";
+}
+
+function debugException(Throwable $e): void
+{
+    debug([
+        'message' => $e->getMessage(),
+        'code' => $e->getCode(),
+        'file' => $e->getFile(),
+        'line' => $e->getLine(),
+    ], 'error');
+}
